d1ea166d89421b9aa15d58593130c8cf49a49db1
vaibhavs572 <vaibhavsharma572@yahoo.com>
2019-12-03 15:51:25 +0530

    Components: Created 'Header' Component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Header = function() {
+    return (
+        <div className="header">
+            Phone Directory
+        </div>
+    )
+}
+
+export default Header; 
